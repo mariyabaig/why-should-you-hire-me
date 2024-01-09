31babfbd07ca19afdaa0040470da068564fe7110
mariyabaig <mariyabaig0106@gmail.com>
2024-01-09 18:49:15 +0530

    optimized

--- script.js
+++ script.js
@@ -1,86 +1,25 @@
-const question = document.querySelector(".question");
-const gif = document.querySelector(".gif");
-const yesBtn = document.querySelector(".yes-btn");
-const noBtn = document.querySelector(".no-btn");
-const messageContainer = document.querySelector(".message-container");
-const messageContainerTwo = document.querySelector(".message-container-two");
-const messageContainerThree = document.querySelector(".message-container-three");
-const messageContainerFour = document.querySelector(
-  ".message-container-four"
-);
-const messageContainerFifth = document.querySelector(".message-container-fifth");
+const elements = {
+  question: document.querySelector(".question"),
+  gif: document.querySelector(".gif"),
+  yesBtn: document.querySelector(".yes-btn"),
+  noBtn: document.querySelector(".no-btn"),
+  messageContainers: [
+    document.querySelector(".message-container"),
+    document.querySelector(".message-container-two"),
+    document.querySelector(".message-container-three"),
+    document.querySelector(".message-container-four"),
+    document.querySelector(".message-container-fifth"),
+    document.querySelector(".message-container-sixth"),
+  ],
+};
 
-const messageContainerSixth = document.querySelector(
-  ".message-container-sixth"
-);
+const timeouts = [];
 
-let timeoutMessage;
-let timeoutSecondMessage;
-let timeoutThirdMessage;
-const maxScreenTime = 2000; 
-const messageDisplayTime = 2000;
-const maxScreenTimeTwo = 8000;
-const messageDisplayTimeTwo = 5000;
-const maxScreenTimeThree = 8000;
-const messageDisplayTimeThree = 5000;
-const maxScreenTimeFour = 2000;
-const messageDisplayTimeFour = 8000;
-const maxScreenTimeFifth = 3000; 
-const messageDisplayTimeFifth = 5000;
-const maxScreenTimeSixth = 8000;
-const messageDisplayTimeSixth = 5000;
+const maxScreenTimes = [2000, 8000, 8000, 2000, 3000, 8000];
+const messageDisplayTimes = [2000, 5000, 5000, 8000, 5000, 5000];
 
-function showActFastMessage() {
-  showMessage(
-    "What's taking you so long?",
-    "https://media.giphy.com/media/mCgALMhfxiZn4oz5P5/giphy.gif",
-    messageContainer,
-    messageDisplayTime
-  );
-}
-
-function showWhatMessage() {
-  showMessage("I'm skilled in JavaScript, React.js, Next.js & TypeScript.", "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnZ1NnM4c25jdmJrNW90NTdqc2I1bXd2b21udTh4MGhyajdxdGs2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/JQRTxak7sNdwOmB1DP/giphy.gif",messageContainerTwo, messageDisplayTimeTwo);
-}
-
-function showThirdMessage() {
-  showMessage(
-    "I can also learn and adapt quickly",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjJ0bzY5cTBxYW4wb3F1dWtqZGxndm8xcHY5ZHh0NmJwa2c0enRoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/NB5Ato4gbETeE6SkKh/giphy.gif",
-    messageContainerThree,
-    messageDisplayTimeThree
-  );
-}
-
-function showFourthMessage() {
-  showMessage(
-    "I am also very hardworking",
-    "https://media.giphy.com/media/gFisgTI9VHKN6gUAnv/giphy.gif",
-    messageContainerFour,
-    messageDisplayTimeFour
-  );
-}
-
-function showFifthMessage() {
-  showMessage(
-    "I loooove networking with people as well!",
-    "https://media.giphy.com/media/UhdWrWgDPiRUjiNyRZ/giphy.gif",
-    messageContainerFifth,
-    messageDisplayTimeFifth
-  );
-}
-
-function showSixthMessage() {
-  showMessage(
-    "I am a very positive and funny being, will make the workplace even more fun!",
-    "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3NlZTJydTc2dXB6aWYzaGc2emFzcWh3bDA2ZmIwc3lxbDVuZm16dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/lOkABr28eLcZgYXq5y/giphy.gif",
-    messageContainerSixth,
-    messageDisplayTimeSixth
-  );
-}
-
-
-function showMessage(messageText, gifSrc, container, displayTime) {
+function showMessage(messageText, gifSrc, containerIndex) {
+  const container = elements.messageContainers[containerIndex];
   const isMessageDisplayed = container.querySelector("p");
 
   if (!isMessageDisplayed) {
@@ -94,85 +33,60 @@ function showMessage(messageText, gifSrc, container, displayTime) {
     container.appendChild(messageParagraph);
     container.appendChild(messageGif);
 
-    setTimeout(() => {
-      container.innerHTML = "";
-    }, displayTime);
+    const displayTime = messageDisplayTimes[containerIndex];
 
-    setTimeout(() => {
-      container.style.display = "none";
-    }, displayTime);
+    timeouts.push(
+      setTimeout(() => {
+        container.innerHTML = "";
+        container.style.display = "none";
+      }, displayTime)
+    );
   }
 }
 
-
 function clearAllTimeouts() {
-  clearTimeout(timeoutMessage);
-  clearTimeout(timeoutSecondMessage);
-  clearTimeout(timeoutThirdMessage);
-  clearTimeout(timeoutFourthMessage);
-  clearTimeout(timeoutFifthMessage);
-  clearTimeout(timeoutSixthMessage);
-  messageContainer.style.display = "none";
-  messageContainerTwo.style.display = "none";
-  messageContainerThree.style.display = "none";
-  messageContainerFour.style.display = "none";
-  messageContainerFifth.style.display = "none";
-  messageContainerSixth.style.display = "none";
+  timeouts.forEach(clearTimeout);
+  elements.messageContainers.forEach(
+    (container) => (container.style.display = "none")
+  );
 }
 
 function startTimeoutMessage() {
-  timeoutMessage = setTimeout(() => {
-    showActFastMessage();
-
-    timeoutSecondMessage = setTimeout(() => {
-      showWhatMessage();
-
-      timeoutThirdMessage = setTimeout(() => {
-        showThirdMessage();
-
-        timeoutFourthMessage = setTimeout(() => {
-          showFourthMessage();
-
-          timeoutFifthMessage = setTimeout(() => {
-            showFifthMessage();
-
-            timeoutSixthMessage = setTimeout(() => {
-              showSixthMessage();
-            }, messageDisplayTimeSixth);  
-          }, messageDisplayTimeFifth);
-        }, messageDisplayTimeFour);
-      }, messageDisplayTimeThree);
-    }, maxScreenTimeTwo);
-  }, maxScreenTime);
+  maxScreenTimes.reduce((prevTimeout, maxScreenTime, index) => {
+    const timeout = setTimeout(() => {
+      showMessage(messages[index].text, messages[index].gif, index);
+    }, prevTimeout + maxScreenTime);
+    timeouts.push(timeout);
+    return prevTimeout + maxScreenTime;
+  }, 0);
 }
 
-
-yesBtn.addEventListener("click", () => {
-   clearAllTimeouts()
-  question.innerHTML = "Yayy, thank you!";
-  gif.src = "https://media.giphy.com/media/4RJBOuC45ptdMszWvt/giphy.gif";
-  yesBtn.style.display = "none";
-  noBtn.style.display = "none";
-
+elements.yesBtn.addEventListener("click", () => {
+  clearAllTimeouts();
+  elements.question.innerHTML = "Yayy, thank you!";
+  elements.gif.src =
+    "https://media.giphy.com/media/4RJBOuC45ptdMszWvt/giphy.gif";
+  elements.yesBtn.style.display = "none";
+  elements.noBtn.style.display = "none";
 });
 
-noBtn.addEventListener("mouseover", () => {
-
-  const noBtnRect = noBtn.getBoundingClientRect();
+elements.noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = elements.noBtn.getBoundingClientRect();
   const maxX = window.innerWidth - noBtnRect.width;
   const maxY = window.innerHeight - noBtnRect.height;
 
   const randomX = Math.floor(Math.random() * maxX);
   const randomY = Math.floor(Math.random() * maxY);
 
-  noBtn.style.left = randomX + "px";
-  noBtn.style.top = randomY + "px";
+  elements.noBtn.style.left = randomX + "px";
+  elements.noBtn.style.top = randomY + "px";
 
   startTimeoutMessage();
 });
+
 document.addEventListener("DOMContentLoaded", function () {
-  var gifImage = document.querySelector(".gif");
-  var question = document.querySelector(".question");
+  const gifImage = elements.gif;
+  const question = elements.question;
 
   setTimeout(function () {
     gifImage.src = "https://media.giphy.com/media/BmfDRHdpLw6sszpKp9/giphy.gif";
@@ -189,6 +103,31 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 3000);
 });
 
-
+const messages = [
+  {
+    text: "What's taking you so long?",
+    gif: "https://media.giphy.com/media/mCgALMhfxiZn4oz5P5/giphy.gif",
+  },
+  {
+    text: "I'm skilled in JavaScript, React.js, Next.js & TypeScript.",
+    gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNnZ1NnM4c25jdmJrNW90NTdqc2I1bXd2b21udTh4MGhyajdxdGs2YSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/JQRTxak7sNdwOmB1DP/giphy.gif",
+  },
+  {
+    text: "I can also learn and adapt quickly",
+    gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMjJ0bzY5cTBxYW4wb3F1dWtqZGxndm8xcHY5ZHh0NmJwa2c0enRoMCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/NB5Ato4gbETeE6SkKh/giphy.gif",
+  },
+  {
+    text: "I am also very hardworking",
+    gif: "https://media.giphy.com/media/gFisgTI9VHKN6gUAnv/giphy.gif",
+  },
+  {
+    text: "I loooove networking with people as well!",
+    gif: "https://media.giphy.com/media/UhdWrWgDPiRUjiNyRZ/giphy.gif",
+  },
+  {
+    text: "I am a very positive and funny being, will make the workplace even more fun!",
+    gif: "https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExd3NlZTJydTc2dXB6aWYzaGc2emFzcWh3bDA2ZmIwc3lxbDVuZm16dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/lOkABr28eLcZgYXq5y/giphy.gif",
+  },
+];
 
 startTimeoutMessage();
